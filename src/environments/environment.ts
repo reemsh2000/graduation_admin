525fb734637c01008668bfc8d4223022a43ab6d3
reemsh2000 <sh951652@gmail.com>
2023-01-23 09:58:10 +0200

    edit auth profile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDaOdXLP0G-3CK70oMKrxuHHrhIMIG4LsI',
-    authDomain: 'nulla-316b1.firebaseapp.com',
-    projectId: 'nulla-316b1',
-    storageBucket: 'nulla-316b1.appspot.com',
-    messagingSenderId: '687533639630',
-    appId: '1:687533639630:web:a0e61408bfe513af5dcd9f',
+    apiKey: "AIzaSyBajBy2LILo3cTAk6QvGtP2T2daoDdXboA",
+    authDomain: "graduation-175d5.firebaseapp.com",
+    projectId: "graduation-175d5",
+    storageBucket: "graduation-175d5.appspot.com",
+    messagingSenderId: "426431154937",
+    appId: "1:426431154937:web:984d51acb0a92edfc5f028"
   },
   FounderEmail:"charlotte@nula.work",
 };
