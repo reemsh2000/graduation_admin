eac133f6ebc22742561773727ce663f4fe475ad6
reemsh2000 <sh951652@gmail.com>
2023-01-23 09:52:32 +0200

    add firebase config

--- functions/src/firebase.js
+++ functions/src/firebase.js
@@ -1,7 +1,12 @@
 import * as admin from "firebase-admin";
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyBajBy2LILo3cTAk6QvGtP2T2daoDdXboA",
+    authDomain: "graduation-175d5.firebaseapp.com",
+    projectId: "graduation-175d5",
+    storageBucket: "graduation-175d5.appspot.com",
+    messagingSenderId: "426431154937",
+    appId: "1:426431154937:web:984d51acb0a92edfc5f028"
 };
 
 admin.initializeApp(firebaseConfig);
