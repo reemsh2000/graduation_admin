fd6b7ac34644e7faa2267d2b00fad7531a778f1f
reemsh2000 <sh951652@gmail.com>
2023-01-25 09:57:50 +0200

    edit auth functions

--- src/app/pages/Auth/login/login.component.ts
+++ src/app/pages/Auth/login/login.component.ts
@@ -1,37 +1,32 @@
-import { environment } from '../../../../environments/environment';
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'app/services/auth.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { environment } from "../../../../environments/environment";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "app/services/auth.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  profileForm = new FormGroup({
-    email: new FormControl('', Validators.email),
-    password: new FormControl('', Validators.minLength(6)),
-  });
-  massage: string = '';
-  get f() {
-    return this.profileForm.controls;
-  }
-  constructor(
-    private authService: AuthService,
-  ) {}
+	profileForm = new FormGroup({
+		email: new FormControl("", Validators.email),
+		password: new FormControl("", Validators.minLength(6)),
+	});
+	massage: string = "";
+	get f() {
+		return this.profileForm.controls;
+	}
+	constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
-  login() {
-    if (this.profileForm.valid) {
-      this.authService.login(this.profileForm.value);
-      this.authService.errorMsg$.subscribe((msg) => {
-        this.massage = msg;
-      });
-    } else {
-      this.massage = 'You should enter email and password';
-    }
-  }
-  loginWithGoogle() {
-    // this.authService.loginWithGoogle();
-  }
+	ngOnInit(): void {}
+	async login() {
+		if (this.profileForm.valid) {
+			await this.authService.login(this.profileForm.value);
+			this.authService.errorMsg$.subscribe((msg) => {
+				this.massage = msg;
+			});
+		} else {
+			this.massage = "You should enter email and password";
+		}
+	}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,103 +1,138 @@
-import { Injectable } from "@angular/core";
-import { AngularFireAuth } from "@angular/fire/compat/auth";
-import { AngularFirestore } from "@angular/fire/compat/firestore";
-import { Router } from "@angular/router";
-import { BehaviorSubject } from "rxjs";
+import { Injectable, OnInit } from '@angular/core'
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFirestore } from '@angular/fire/compat/firestore'
+import { Router } from '@angular/router'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
-	providedIn: "root",
+  providedIn: 'root',
 })
-export class AuthService {
-	loginValue: boolean;
-	completeform: boolean;
-	adminInfo: any;
-	item: any;
-	constructor(private auth: AngularFireAuth, private router: Router, public firestore: AngularFirestore) {}
-	private errorMsg = new BehaviorSubject<string>("");
-	public errorMsg$ = this.errorMsg.asObservable();
-	private email = new BehaviorSubject<any>("");
-	public email$ = this.email.asObservable();
+export class AuthService implements OnInit {
+  loginValue: boolean
+  completeform: boolean
+  adminInfo: any
+  item: any
+  admins: any = []
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    public firestore: AngularFirestore,
+  ) {}
+  async ngOnInit() {
+    await this.getAdmins()
+  }
+  private errorMsg = new BehaviorSubject<string>('')
+  public errorMsg$ = this.errorMsg.asObservable()
+  private email = new BehaviorSubject<any>('')
+  public email$ = this.email.asObservable()
 
-	private username = new BehaviorSubject<any>(null);
-	public username$ = this.username.asObservable();
+  private username = new BehaviorSubject<any>(null)
+  public username$ = this.username.asObservable()
 
-	private profileData = new BehaviorSubject<any>(null);
-	public profileData$ = this.profileData.asObservable();
+  private profileData = new BehaviorSubject<any>(null)
+  public profileData$ = this.profileData.asObservable()
 
-	public get getErrorMsg(): string {
-		return this.errorMsg.getValue();
-	}
+  public get getErrorMsg(): string {
+    return this.errorMsg.getValue()
+  }
 
-	userId = "";
+  userId = ''
 
-	register(form: any, Record: any) {
-		this.auth["createUserWithEmailAndPassword"](form.email, form.password).then((res: { user: any }) => {
-			this.userId = res.user.uid;
-			this.firestore
-				.collection("admins")
-				.doc(res.user.uid)
-				.set({ name: Record.name })
-				.then(() => {
-					this.username.next(Record.name);
-					this.email.next(form.email);
-					this.router.navigate(["/dashboard"]);
-				});
-		});
-	}
-	login(form: any) {
-		this.auth["signInWithEmailAndPassword"](form.email, form.password)
-			.then((res: { user: any }) => {
-				this.userId = res.user.uid;
-				this.email.next(form.email);
-				this.getUserData();
-				this.router.navigate(["/dashboard"]);
-			})
-			.catch((err) => {
-				this.errorMsg.next("Invalid Email or Password");
-			});
-		this.router.navigate(["/dashboard"]);
-	}
+  register(form: any, Record: any) {
+    this.auth['createUserWithEmailAndPassword'](form.email, form.password).then(
+      (res: { user: any }) => {
+        this.userId = res.user.uid
+        this.firestore
+          .collection('admins')
+          .doc(res.user.uid)
+          .set({ name: Record.name, email: form.email })
+          .then(() => {
+            this.username.next(Record.name)
+            this.email.next(form.email)
+            this.router.navigate(['/dashboard'])
+          })
+      },
+    )
+  }
+  async login(form: any) {
+    try {
+      await this.getAdmins()
+      const res = await this.auth['signInWithEmailAndPassword'](
+        form.email,
+        form.password,
+      )
+      this.userId = res.user?.uid!
+      this.email.next(form.email)
+      await this.getUserData()
+      const checkIfAdmin = this.admins.find(
+        (admin: string) => admin === form.email,
+      )
+      console.log({ admins: this.admins })
+      if (checkIfAdmin) {
+        this.router.navigate(['/dashboard'])
+      } else {
+        this.errorMsg.next('This user is not admin')
+      }
+    } catch (error) {
+      this.errorMsg.next('Invalid Email or Password')
+    }
+  }
 
-	ResetPassword(email: string) {
-		let msg;
-		return this.auth
-			.sendPasswordResetEmail(email)
-			.then(
-				() => {
-					msg = "A password reset link has been sent to your email address , check spam emails";
+  ResetPassword(email: string) {
+    let msg
+    return this.auth
+      .sendPasswordResetEmail(email)
+      .then(
+        () => {
+          msg =
+            'A password reset link has been sent to your email address , check spam emails'
 
-					return {
-						detail: msg,
-						summary: "Success",
-						severity: "success",
-					};
-				},
-				(rejectionReason) => {
-					msg = "There is no user record corresponding to this identifier.";
-					return rejectionReason && { detail: msg, severity: "error", summary: "Error" };
-				}
-			)
-			.catch((e) => {
-				msg = "An error occurred while attempting to reset your password";
-				return { detail: msg, severity: "error", summary: "Error" };
-			});
-	}
+          return {
+            detail: msg,
+            summary: 'Success',
+            severity: 'success',
+          }
+        },
+        (rejectionReason) => {
+          msg = 'There is no user record corresponding to this identifier.'
+          return (
+            rejectionReason && {
+              detail: msg,
+              severity: 'error',
+              summary: 'Error',
+            }
+          )
+        },
+      )
+      .catch((e) => {
+        msg = 'An error occurred while attempting to reset your password'
+        return { detail: msg, severity: 'error', summary: 'Error' }
+      })
+  }
 
-	logout() {
-		this.auth.signOut().then(() => {
-			this.router.navigate(["/login"]);
-			this.loginValue = false;
-		});
-	}
+  logout() {
+    this.auth.signOut().then(() => {
+      this.router.navigate(['/login'])
+      this.loginValue = false
+    })
+  }
 
-	getUserData() {
-		return this.firestore
-			.collection("admins")
-			.doc(this.userId)
-			.get()
-			.subscribe((data) => {
-				this.username.next(data.data());
-			});
-	}
+  getUserData() {
+    return this.firestore
+      .collection('admins')
+      .doc(this.userId)
+      .get()
+      .subscribe((data) => {
+        this.username.next(data.data())
+      })
+  }
 
+  getAdmins() {
+    return this.firestore
+      .collection('admins')
+      .get()
+      .subscribe((data) => {
+        data.docs.map((ele: any) => this.admins.push(ele.data().email))
+      })
+  }
 }
