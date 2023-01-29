17c97cf93f4d7e4c19ed168f5159010f3a49ef43
reemsh2000 <sh951652@gmail.com>
2023-01-29 08:53:15 +0200

    add new input

--- src/app/pages/add-exercise/add-exercise.component.ts
+++ src/app/pages/add-exercise/add-exercise.component.ts
@@ -1,45 +1,68 @@
-import { AsideService } from "app/services/aside.service";
-import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
-import { DataService } from "app/services/data.service";
-import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
+import { AsideService } from 'app/services/aside.service'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { DataService } from 'app/services/data.service'
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject'
 
 @Component({
-	selector: "app-profile",
-	templateUrl: "./add-exercise.component.html",
-	styleUrls: ["./add-exercise.component.css"],
+  selector: 'app-profile',
+  templateUrl: './add-exercise.component.html',
+  styleUrls: ['./add-exercise.component.css'],
 })
 export class AddExerciseComponent implements OnInit {
-	completeformprofile: boolean;
-	showMsg: boolean = false;
-	bodyPart = ["back", "cardio", "chest", "lower arms", "lower legs", "neck", "shoulders", "upper arms", "upper legs", "waist"];
-	excersiseForm = new FormGroup({
-		name: new FormControl("", Validators.required),
-		urlVideo: new FormControl("", Validators.required),
-		description: new FormControl("", Validators.required),
-		restrictions: new FormControl([]),
-		// bodyPart: new FormControl([], Validators.required),
-	});
-	private msg = new BehaviorSubject<any>({});
-	public msg$ = this.msg.asObservable();
-	constructor(private dataService: DataService, private router: Router, public asideService: AsideService) {
-		this.asideService.setSection("Add Exercise");
-	}
+  completeformprofile: boolean
+  showMsg: boolean = false
+  bodyPart = [
+    { name: 'back' },
+    { name: 'cardio' },
+    { name: 'chest' },
+    { name: 'lower arms' },
+    { name: 'lower legs' },
+    { name: 'neck' },
+    { name: 'shoulders' },
+    { name: 'upper arms' },
+    { name: 'upper legs' },
+    { name: 'waist' },
+  ]
+  excersiseForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    urlVideo: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    restrictions: new FormControl([]),
+    muscleName: new FormControl('', Validators.required),
+  })
+  private msg = new BehaviorSubject<any>({})
+  public msg$ = this.msg.asObservable()
+  restrictions: any
 
-	ngOnInit(): void {}
-	async save() {
-		if (this.excersiseForm.valid) {
-			let message = await this.dataService.addExcersise(this.excersiseForm.value);
-			this.msg.next(message);
-			this.excersiseForm.reset();
-		} else {
-			this.msg.next({
-				detail: "You should fill the required fields",
-				summary: "Warn",
-				severity: "warn",
-			});
-		}
-		this.showMsg = true;
-	}
+  constructor(
+    private dataService: DataService,
+    public asideService: AsideService,
+  ) {
+    this.asideService.setSection('Add Exercise')
+  }
+
+  ngOnInit(): void {}
+  async save() {
+    console.log({ form: this.excersiseForm.value })
+    const formData = {
+      ...this.excersiseForm.value,
+      muscleName: this.excersiseForm.value.muscleName.name,
+    }
+    console.log({ formData })
+
+    if (this.excersiseForm.valid) {
+      let message = await this.dataService.addExcersise(formData)
+      this.msg.next(message)
+      this.excersiseForm.reset()
+    } else {
+      this.msg.next({
+        detail: 'You should fill the required fields',
+        summary: 'Warn',
+        severity: 'warn',
+      })
+    }
+    this.showMsg = true
+  }
 }
