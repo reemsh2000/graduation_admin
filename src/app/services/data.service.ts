c901980a1808a6e87598aec7e8fd43f8dc84df03
reemsh2000 <sh951652@gmail.com>
2023-02-03 19:12:14 +0200

    edit add new doctot function

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { Router } from '@angular/router'
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { BehaviorSubject } from 'rxjs'
 
 @Injectable({
@@ -10,13 +11,17 @@ export class DataService {
   private exercises = new BehaviorSubject<any>([])
   public exercises$ = this.exercises.asObservable()
 
-  constructor(private router: Router, public firestore: AngularFirestore) {}
+  constructor(
+    private router: Router,
+    public firestore: AngularFirestore,
+    private auth: AngularFireAuth,
+  ) {}
   addExercise(Record: any): any {
     let exercises = { ...Record, id: this.firestore.createId() }
     return this.firestore
       .collection('exercises')
       .doc(exercises.id)
-      .set({ Record: exercises })
+      .set({ ...exercises })
       .then(() => {
         const msg = 'exercises added successfully'
 
@@ -37,7 +42,7 @@ export class DataService {
       .collection('exercises')
       .get()
       .subscribe((data: any) => {
-        data.docs.map((ele: any) => exercises.push(ele.data().Record))
+        data.docs.map((ele: any) => exercises.push(ele.data()))
         this.exercises.next(exercises)
       })
   }
@@ -50,26 +55,32 @@ export class DataService {
     return this.firestore
       .collection('exercises')
       .doc(id)
-      .update({ Record: { ...exercises, id } })
+      .update({ ...exercises, id })
   }
   deleteExercises(id: string) {
     return this.firestore.collection('exercises').doc(id).delete()
   }
 
-  addDoctorEmail(form: any) {
-    return this.firestore
-      .collection('doctors')
-      .add({ name: form.name, doctorEmail: form.doctorEmail })
-      .then(() => {
-        const msg = 'Doctor added successfully'
-        return {
-          detail: msg,
-          severity: 'success',
-          summary: 'Success',
-        }
-      })
-      .catch((err) => {
-        console.error(err)
-      })
+  addNewDoctor(form: any) {
+    return this.auth['createUserWithEmailAndPassword'](
+      form.email,
+      'doctor123',
+    ).then((res: { user: any }) => {
+     return this.firestore
+        .collection('doctors')
+        .doc(res.user.uid)
+        .set({ name: form.name, email: form.email, id: res.user.uid })
+        .then(() => {
+          const msg = 'Doctor added successfully'
+          return {
+            detail: msg,
+            severity: 'success',
+            summary: 'Success',
+          }
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
   }
 }
